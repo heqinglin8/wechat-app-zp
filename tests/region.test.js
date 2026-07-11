08226802676da4796452e977dc6ecd3ddc2de840
heqinglin <237607591@qq.com>
2026-07-11 10:14:45 +0800

    完成城市信息查询代码开发

---
+++ tests/region.test.js
@@ -0,0 +1,87 @@
+var assert = require('assert');
+var region = require('../utils/region.js');
+
+var result = region.findByDistrictCode('110102');
+
+assert.deepStrictEqual(result, {
+  provinceCode: '110000',
+  provinceName: '北京市',
+  cityCode: '110100',
+  cityName: '市辖区',
+  districtCode: '110102',
+  districtName: '西城区',
+});
+
+var displayResult = region.findDisplayByDistrictCode('110102');
+
+assert.deepStrictEqual(displayResult, {
+  provinceCode: '110000',
+  provinceName: '北京市',
+  cityCode: '110100',
+  cityName: '北京市',
+  districtCode: '110102',
+  districtName: '西城区',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('500101'), {
+  provinceCode: '500000',
+  provinceName: '重庆市',
+  cityCode: '500100',
+  cityName: '重庆市',
+  districtCode: '500101',
+  districtName: '万州区',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('150103'), {
+  provinceCode: '150000',
+  provinceName: '内蒙古',
+  cityCode: '150100',
+  cityName: '呼和浩特市',
+  districtCode: '150103',
+  districtName: '回民区',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('659001'), {
+  provinceCode: '650000',
+  provinceName: '新疆',
+  cityCode: '659000',
+  cityName: '自治区直辖县级行政区划',
+  districtCode: '659001',
+  districtName: '石河子市',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('450102'), {
+  provinceCode: '450000',
+  provinceName: '广西',
+  cityCode: '450100',
+  cityName: '南宁市',
+  districtCode: '450102',
+  districtName: '兴宁区',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('540102'), {
+  provinceCode: '540000',
+  provinceName: '西藏',
+  cityCode: '540100',
+  cityName: '拉萨市',
+  districtCode: '540102',
+  districtName: '城关区',
+});
+
+assert.deepStrictEqual(region.findDisplayByDistrictCode('640104'), {
+  provinceCode: '640000',
+  provinceName: '宁夏',
+  cityCode: '640100',
+  cityName: '银川市',
+  districtCode: '640104',
+  districtName: '兴庆区',
+});
+
+console.log('region.findByDistrictCode("110102") passed');
+console.log('region.findDisplayByDistrictCode("110102") passed');
+console.log('region.findDisplayByDistrictCode("500101") passed');
+console.log('region.findDisplayByDistrictCode("150103") passed');
+console.log('region.findDisplayByDistrictCode("659001") passed');
+console.log('region.findDisplayByDistrictCode("450102") passed');
+console.log('region.findDisplayByDistrictCode("540102") passed');
+console.log('region.findDisplayByDistrictCode("640104") passed');
---
+++ utils/region.js
@@ -0,0 +1,104 @@
+var pcaCode = require('../assets/pca-code.json');
+
+var metadata = {
+  localFile: 'assets/pca-code.json',
+  sourceName: '国家统计局 统计用区划代码和城乡划分代码',
+  sourceUrl: 'https://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/',
+  snapshotYear: '2023',
+  level: 'province-city-district',
+};
+
+var districtIndex = null;
+
+var AUTONOMOUS_REGION_DISPLAY_NAMES = {
+  '新疆维吾尔自治区': '新疆',
+  '内蒙古自治区': '内蒙古',
+  '广西壮族自治区': '广西',
+  '西藏自治区': '西藏',
+  '宁夏回族自治区': '宁夏',
+};
+
+function normalizeInputCode(value) {
+  return String(value || '').trim();
+}
+
+function normalizeLevelCode(code) {
+  var text = normalizeInputCode(code);
+  if (!/^\d{2,6}$/.test(text)) return '';
+  return (text + '000000').slice(0, 6);
+}
+
+function cloneRegion(region) {
+  if (!region) return null;
+  return {
+    provinceCode: region.provinceCode,
+    provinceName: region.provinceName,
+    cityCode: region.cityCode,
+    cityName: region.cityName,
+    districtCode: region.districtCode,
+    districtName: region.districtName,
+  };
+}
+
+function buildDistrictIndex() {
+  var index = {};
+
+  (pcaCode || []).forEach(function (province) {
+    var provinceCode = normalizeLevelCode(province && province.code);
+    var provinceName = (province && province.name) || '';
+    if (!provinceCode || !provinceName) return;
+
+    (province.children || []).forEach(function (city) {
+      var cityCode = normalizeLevelCode(city && city.code);
+      var cityName = (city && city.name) || '';
+      if (!cityCode || !cityName) return;
+
+      (city.children || []).forEach(function (district) {
+        var districtCode = normalizeInputCode(district && district.code);
+        var districtName = (district && district.name) || '';
+        if (!/^\d{6}$/.test(districtCode) || !districtName) return;
+
+        index[districtCode] = {
+          provinceCode: provinceCode,
+          provinceName: provinceName,
+          cityCode: cityCode,
+          cityName: cityName,
+          districtCode: districtCode,
+          districtName: districtName,
+        };
+      });
+    });
+  });
+
+  return index;
+}
+
+function getDistrictIndex() {
+  if (!districtIndex) {
+    districtIndex = buildDistrictIndex();
+  }
+  return districtIndex;
+}
+
+function findByDistrictCode(districtCode) {
+  var code = normalizeInputCode(districtCode);
+  if (!/^\d{6}$/.test(code)) return null;
+  return cloneRegion(getDistrictIndex()[code]);
+}
+
+function findDisplayByDistrictCode(districtCode) {
+  var region = findByDistrictCode(districtCode);
+  if (region && /自治区$/.test(region.provinceName)) {
+    region.provinceName = AUTONOMOUS_REGION_DISPLAY_NAMES[region.provinceName] || region.provinceName;
+  }
+  if (region && region.cityName === '市辖区') {
+    region.cityName = region.provinceName;
+  }
+  return region;
+}
+
+module.exports = {
+  metadata: metadata,
+  findByDistrictCode: findByDistrictCode,
+  findDisplayByDistrictCode: findDisplayByDistrictCode,
+};
